520ef1d86021ef820bbcce0932de9ec8d34289db
Juan Pablo Montoya Ospinal <juanpablomontoya618@gmail.com>
2024-11-02 21:51:41 -0500

    :european_castle: Add Jacoco and SonarQube
    * Code scanning tools added for code testing, coverage and Quality Gates. Jacoco with reporting task configuration and coverage verification, SonarQube for QualityGates validation.
    * An in-memory h2 database added to pull up application context in tests.

--- src/test/java/com/challenge/ecommerce/tps/user_management/UserManagementApplicationTests.java
+++ src/test/java/com/challenge/ecommerce/tps/user_management/UserManagementApplicationTests.java
@@ -2,8 +2,10 @@ package com.challenge.ecommerce.tps.user_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest()
+@ActiveProfiles("test")
 class UserManagementApplicationTests {
 
 	@Test
